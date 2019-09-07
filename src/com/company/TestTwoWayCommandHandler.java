be1c2b6c824db318cb69dd93c7347dbb82b8b860
h.sanei <h.sanei@choganit.ir>
2019-09-07 20:54:51 +0430

    Add Second Type for test

--- src/com/company/TestTwoWayCommandHandler.java
+++ src/com/company/TestTwoWayCommandHandler.java
@@ -1,8 +1,9 @@
 package com.company;
 
-public class TestTwoWayCommandHandler implements ITwoWayCommandHandler<TestTwoWayCommand,String> {
+public class TestTwoWayCommandHandler implements ITwoWayCommandHandler<TestTwoWayCommand, String> {
     @Override
     public String Handle(TestTwoWayCommand command) {
-       return "Hello"+ command.FirstName+" "+command.LastName;
+        return "Hello : " + command.FirstName + " " + command.LastName;
     }
 }
+
